437bb8182ea43bc94536ea4d6fa2ba205ff17f4a
Joshua Paul Powers <jpowers4@uvm.edu>
2020-03-05 18:40:16 -0500

    Create install_script.jl

---
+++ macOS_Catalina/install_script.jl
@@ -0,0 +1,18 @@
+using Pkg
+println("Installation of required packages to run this script")
+Pkg.update()
+ENV["PYTHON"]="/usr/local/anaconda3/bin/python"
+ENV["JUPYTER"]="/usr/local/anaconda3/bin/jupyter"
+package_list = ["PyCall", "IJulia", "Mux", "WebIO", "Interact", "Flux", "Calculus", "StatsBase", "HypothesisTests",
+"BlackBoxOptim", "BlackBoxOptimizationBenchmarking", "ProgressMeter", "DataStructures", "BSON", "ArrayFire",
+"PackageCompiler", "AbstractPlotting", "GDAL", "MakieGallery", "RDatasets", "Makie", "FileIO", "Colors", "GeometryTypes",
+"LinearAlgebra", "DataFrames", "CSV", "Plots", "ORCA", "PlotlyJS", "PyPlot", "GraphRecipes", "StatsPlots", "LightGraphs",
+"Cxx", "Libdl"]
+for package in package_list
+    if ! in(package,keys(Pkg.installed())) Pkg.add(package) end
+end
+Pkg.build("PyCall")
+Pkg.build("IJulia")
+Pkg.update()
+using WebIO
+WebIO.install_jupyter_nbextension()
