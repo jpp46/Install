1cfc7020f450b4edf43a2be02441ccc570425a6e
Joshua Paul Powers <jpowers4@uvm.edu>
2020-03-05 18:43:35 -0500

    Update mac_install_part1.sh

--- macOS_Catalina/mac_install_part1.sh
+++ macOS_Catalina/mac_install_part1.sh
@@ -99,53 +99,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rm $HOME/.profile
 
 # FINAL PROFILE
-echo \
-"
-#PATH VARIABLES
-export PATH=$HOME/.cargo/bin:/usr/local/anaconda3/bin:/usr/local/bin:/usr/local/sbin:\$PATH
-
-export LD_LIBRARY_PATH=/usr/local/anaconda3/lib:\$LD_LIBRARY_PATH
-
-# Get on the uvm super computer
-alias vacc='ssh jpowers4@bluemoon-user1.uvm.edu'
-
-proj() {
-  cd /Users/powers/Documents/Joshua/Github/\$1
-}
-
-ml() {
-  cd /Users/powers/Documents/Joshua/Learning/Udemy/MLCourse
-  jupyter-notebook
-}
-
-submit() {
-  exercism submit \$1
-}
-
-# Push to github with commit message
-push() {
-  git add .
-  git commit -m \"\$1\"
-  git push -u origin master
-}
-
-findAll() {
-  ps aux | grep \$1
-}
-
-killAll() {
-  kill \$(ps aux | grep \$1 | grep -v grep | awk '{print \$2}')
-}
-
-# Show hidden files in finder
-alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
-
-# Hide hidden files in finder
-alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-
-# Create install media
-makedrive() {
-  sudo \"/Applications/Install macOS Catalina.app/Contents/Resources/createinstallmedia\" --volume /Volumes/\$1
-}" \
-> $HOME/.zprofile
-
+cd Install/macOS_Catalina
+cp .zprofile $HOME
