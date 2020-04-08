fdb4d2ca219ef192be76b3436b3a1a47002279bf
jpp46 <jpowers4@uvm.edu>
2020-04-08 19:19:37 -0400

    updating julia install script

--- macOS_Catalina/install_script.jl
+++ macOS_Catalina/install_script.jl
@@ -7,7 +7,7 @@ ENV["JUPYTER"]="/usr/local/anaconda3/bin/jupyter"
 package_list = [
 	"PyCall", "IJulia", "Mux", "WebIO", "Interact", "Flux", "Calculus", "StatsBase", "HypothesisTests",
 	"BlackBoxOptim", "BlackBoxOptimizationBenchmarking", "ProgressMeter", "DataStructures", "BSON", "ArrayFire",
-	"PackageCompiler", "AbstractPlotting", "GDAL", "MakieGallery", "RDatasets", "Makie", "FileIO", "Colors", "GeometryTypes",
+	"PackageCompiler", "AbstractPlotting", "GDAL", "MakieGallery", "RDatasets", "Makie", "Images", "ImageTransformations", "FileIO", "Colors", "GeometryTypes",
 	"LinearAlgebra", "DataFrames", "CSV", "Plots", "ORCA", "PlotlyJS", "PyPlot", "GraphRecipes", "StatsPlots", "LightGraphs",
 	"Cxx", "Libdl"
 ]
